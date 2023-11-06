97644be12fb2e1affded706187577c5274fc43b6
prarthana-v <p.d.vaghani1729@gmail.com>
2023-11-06 18:22:14 +0530

    c-programs

---
+++ PROGRAM1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+
+clrscr();
+
+	printf("My Name is Vaghani prarthana\n");
+	printf("I'm 19 years old..\n");
+	printf("I'm in full stack course..\n");
+	printf("i have studied in kaushal vidhyabhavan..\n");
+	printf("My GRID is 3931..\n");
+	printf("My Locality is mota varachha,surat..\n");
+getch();
+}
